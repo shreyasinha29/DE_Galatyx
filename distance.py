9e7023c6725fa5523a1230d46d99317ab90d9744
Shreya Sinha <71968547+shreyasinha29@users.noreply.github.com>
2023-11-16 15:29:57 +0530

    Update distance.py

--- distance.py
+++ distance.py
@@ -3,5 +3,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 class DistanceCalculator:
     def calculate_cosine_similarity(self, vector1, vector2):
+        """
+        Calculate cosine similarity between two vectors.
+
+        Parameters:
+        - vector1, vector2: 1D arrays or lists representing vectors.
+
+        Returns:
+        - similarity: Cosine similarity between the two vectors.
+        """
         similarity = cosine_similarity(np.array([vector1]), np.array([vector2]))[0][0]
         return similarity
